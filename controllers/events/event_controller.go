04086b6164a1500c30022bc8a98348589977b9f6
Bryan Boreham <bjboreham@gmail.com>
2020-10-23 17:22:43 +0000

    Count event types in a Prometheus metric

--- controllers/events/event_controller.go
+++ controllers/events/event_controller.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	"github.com/prometheus/client_golang/prometheus"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 // EventWatcher listens to Events
@@ -15,15 +17,43 @@ type EventWatcher struct {
 	Log logr.Logger
 }
 
-func (r *EventWatcher) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("event", req.NamespacedName)
+var (
+	totalEventsNum = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "kspan",
+			Subsystem: "events",
+			Name:      "total",
+			Help:      "The total number of events.",
+		},
+		[]string{"type", "involved_object", "reason"})
+)
 
-	// your logic here
+func init() {
+	// Register custom metrics with the global prometheus registry
+	metrics.Registry.MustRegister(totalEventsNum)
+}
+
+// Reconcile gets called every time an Event changes
+func (r *EventWatcher) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("event", req.NamespacedName)
+
+	var event corev1.Event
+	if err := r.Get(ctx, req.NamespacedName, &event); err != nil {
+		if apierrors.IsNotFound(err) {
+		// we get this on deleted events, which happen all the time; just ignore it.
+			return ctrl.Result{}, nil
+	}
+		log.Error(err, "unable to fetch Event")
+		return ctrl.Result{}, err
+	}
+
+	totalEventsNum.WithLabelValues(event.Type, event.InvolvedObject.Kind, event.Reason).Inc()
 
 	return ctrl.Result{}, nil
 }
 
+// SetupWithManager to set up the watcher
 func (r *EventWatcher) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&corev1.Event{}).
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/go-logr/logr v0.1.0
+	github.com/prometheus/client_golang v1.0.0
 	k8s.io/api v0.17.2
 	k8s.io/apimachinery v0.17.2
 	k8s.io/client-go v0.17.2
