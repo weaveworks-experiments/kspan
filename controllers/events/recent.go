02dba7ec4afbc4f536927fc44da2008ea9561239
Bryan Boreham <bjboreham@gmail.com>
2021-01-28 18:24:56 +0000

    Flesh out recentInfo store
    Currently allowing it to find all data, recent or not.

--- controllers/events/recent.go
+++ controllers/events/recent.go
@@ -13,27 +13,54 @@ const (
 
 type recentInfoStore struct {
 	recentWindow time.Duration // events within this window are considered likely to belong together
-	expireAfter  time.Duration // how long to wait for a parent trace to appear
+	expireAfter  time.Duration // how long to keep events in the recent cache
 
 	info map[parentChild]recentInfo
 }
 
 // Info about what happened recently with an object
 type recentInfo struct {
+	lastUsed time.Time
 	trace.SpanContext
 }
 
 func newRecentInfoStore() recentInfoStore {
 	return recentInfoStore{
-		info: make(map[parentChild]recentInfo),
+		recentWindow: defaultRecentWindow,
+		expireAfter:  defaultExpireAfter,
+		info:         make(map[parentChild]recentInfo),
 	}
 }
 
 func (r *recentInfoStore) store(key parentChild, spanContext trace.SpanContext) {
-	r.info[key] = recentInfo{SpanContext: spanContext}
+	r.info[key] = recentInfo{
+		lastUsed:    time.Now(),
+		SpanContext: spanContext,
+	}
 }
 
 func (r *recentInfoStore) lookupSpanContext(key parentChild) (trace.SpanContext, bool) {
+	now := time.Now()
 	value, ok := r.info[key]
+	if !ok {
+		return noTrace, false
+	}
+	if value.lastUsed.Before(now.Add(-r.recentWindow)) {
+		// fmt.Printf("key %v info too old %s, %s\n", key, value.lastUsed, now.Add(-r.recentWindow))
+		//return noTrace, false
+	}
+	value.lastUsed = now
+	r.info[key] = value
 	return value.SpanContext, ok
+}
+
+func (r *recentInfoStore) expire() {
+	now := time.Now()
+	expiry := now.Add(-r.expireAfter)
+	// lock?
+	for k, v := range r.info {
+		if v.lastUsed.Before(expiry) {
+			delete(r.info, k)
+		}
+	}
 }
