1ba523d76cabebb046d00a2208064ed886310993
Bryan Boreham <bjboreham@gmail.com>
2021-03-18 17:06:31 +0000

    Set the end time of spans
    Hold outgoing spans, and adjust them to be the length of all
    of their children.
    
    Also set the end-time of one span to the start-time of the next one for
    the same object.

---
+++ controllers/events/outgoing.go
@@ -0,0 +1,74 @@
+package events
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	apitrace "go.opentelemetry.io/otel/api/trace"
+	tracesdk "go.opentelemetry.io/otel/sdk/export/trace"
+)
+
+type outgoing struct {
+	sync.Mutex
+
+	byRef    map[objectReference]*tracesdk.SpanData
+	bySpanID map[apitrace.SpanID]*tracesdk.SpanData
+}
+
+func newOutgoing() *outgoing {
+	return &outgoing{
+		byRef:    make(map[objectReference]*tracesdk.SpanData),
+		bySpanID: make(map[apitrace.SpanID]*tracesdk.SpanData),
+	}
+}
+
+const timeFmt = "15:04:05.000"
+
+func (r *EventWatcher) emitSpan(ctx context.Context, ref objectReference, span *tracesdk.SpanData) {
+	r.Log.Info("adding span", "ref", ref, "name", span.Name, "start", span.StartTime.Format(timeFmt), "end", span.EndTime.Format(timeFmt))
+	r.outgoing.Lock()
+	defer r.outgoing.Unlock()
+
+	if prev, found := r.outgoing.byRef[ref]; found {
+		if !prev.StartTime.After(span.StartTime) {
+			prev.EndTime = span.StartTime
+		} else {
+			r.Log.Info("New span before old span", "oldSpan", prev.Name, "oldTime", prev.StartTime.Format(timeFmt), "newSpan", span.Name, "newTime", span.StartTime.Format(timeFmt))
+		}
+		r.Log.Info("emitting span", "ref", ref, "name", prev.Name)
+		r.Exporter.ExportSpans(ctx, []*tracesdk.SpanData{prev})
+		// We do not remove from bySpanID at this time, in case it is needed for parent chains
+	}
+	r.outgoing.byRef[ref] = span
+	r.outgoing.bySpanID[span.SpanContext.SpanID] = span
+
+	for parentID := span.ParentSpanID; parentID.IsValid(); {
+		if parent, found := r.outgoing.bySpanID[parentID]; found {
+			//r.Log.Info("adjusting endtime", "parent", parent.Name, "from", parent.EndTime.Format(timeFmt), "to", span.EndTime.Format(timeFmt))
+			parent.EndTime = span.EndTime
+			parentID = parent.ParentSpanID
+		} else {
+			break
+		}
+	}
+}
+
+func (r *EventWatcher) flushOutgoing(ctx context.Context, threshold time.Time) {
+	r.outgoing.Lock()
+	defer r.outgoing.Unlock()
+	for k, span := range r.outgoing.byRef {
+		if !span.EndTime.After(threshold) {
+			r.Log.Info("deferred emit", "ref", k, "name", span.Name, "endTime", span.EndTime, "threshold", threshold)
+			r.Exporter.ExportSpans(ctx, []*tracesdk.SpanData{span})
+			delete(r.outgoing.byRef, k)
+			delete(r.outgoing.bySpanID, span.SpanContext.SpanID)
+		}
+	}
+	// Now clear out anything old that is still in bySpanID
+	for k, span := range r.outgoing.bySpanID {
+		if !span.EndTime.After(threshold) {
+			delete(r.outgoing.bySpanID, k)
+		}
+	}
+}
