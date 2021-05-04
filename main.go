9283c8d8a454f08ede85a9a02d5b19098b0c7cb6
Bryan Boreham <bjboreham@gmail.com>
2021-05-04 14:44:45 +0000

    Repository moved to weaveworks-experiments
    This happened some weeks ago; this is the code catching up

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	"github.com/bboreham/kspan/controllers/events"
+	"github.com/weaveworks-experiments/kspan/controllers/events"
 	// +kubebuilder:scaffold:imports
 )
 
